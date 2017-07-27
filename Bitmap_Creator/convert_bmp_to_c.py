161a9e6daee86e4f85eba830077537db65414494
alexeystn <alexey.stn@gmail.com>
2017-07-27 23:22:51 +0300

    added new font

--- Bitmap_Creator/convert_bmp_to_c.py
+++ Bitmap_Creator/convert_bmp_to_c.py
@@ -1,14 +1,1 @@
-import bitmap
-
-b = bitmap.BitMap()
-b.filename = 'times'
-b.Load_File()
-
-if 1: 
-    b.Separate_Chars()
-    b.first_char = '+' # '!' or '+'
-    b.Save_Font()
-else:
-    b.Save_Image()
-
-#bitmap.Write_String('!','~')
+import bitmapb = bitmap.BitMap()b.filename = 'medium'b.Load_File()if 1:     b.Separate_Chars()    b.first_char = '!' # '!' or '+'    b.Save_Font()else:    b.Save_Image()#bitmap.Write_String('!','~')
